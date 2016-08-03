a2499fec332d7d7741dfa135da9bedde48610790
jijunwei <jijunwei@witenet.com>
2016-08-03 12:39:29 +0800

    0803002

--- src/Class1.java
+++ src/Class1.java
@@ -2,7 +2,7 @@
  * Created by jjw on 2016/8/3.
  */
 public class Class1 {
-    public static void main(String[] args) {
-
+    lic static void main(String[] args) {
+        System.out.println("001");
     }
 }
